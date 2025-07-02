26e12f4e51b42923c5748a581543cc7abe11ef7e
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2025-07-02 10:05:08 -0300

    Implementar FuncionarioRepository

--- projeto/src/main/java/com/bd/ufrn/projeto/models/Funcionario.java
+++ projeto/src/main/java/com/bd/ufrn/projeto/models/Funcionario.java
@@ -1,14 +1,14 @@
 package com.bd.ufrn.projeto.models;
 
 import com.bd.ufrn.projeto.enums.Papel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 import java.util.List;
 @Getter
 @Setter
-@Builder
+@SuperBuilder
 public class Funcionario extends Pessoa{
     int numFuncionario;
     boolean administrador;
--- projeto/src/main/java/com/bd/ufrn/projeto/models/Hospede.java
+++ projeto/src/main/java/com/bd/ufrn/projeto/models/Hospede.java
@@ -1,12 +1,15 @@
 package com.bd.ufrn.projeto.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
 public class Hospede extends Pessoa{
 
 }
