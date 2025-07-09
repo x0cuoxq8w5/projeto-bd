486e1ddde951e1548dd4b0d8e65a98a690970e31
artur <artur.nobre.114@ufrn.edu.br>
2025-07-09 15:33:06 -0300

    quickfix: esqueci um annotation

--- projeto/src/main/java/com/bd/ufrn/projeto/models/Hospede.java
+++ projeto/src/main/java/com/bd/ufrn/projeto/models/Hospede.java
@@ -1,5 +1,6 @@
 package com.bd.ufrn.projeto.models;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
@@ -7,6 +8,7 @@ import lombok.experimental.SuperBuilder;
 @Getter
 @Setter
 @SuperBuilder
+@Builder
 public class Hospede extends Pessoa{
     private boolean desativado;
 }
