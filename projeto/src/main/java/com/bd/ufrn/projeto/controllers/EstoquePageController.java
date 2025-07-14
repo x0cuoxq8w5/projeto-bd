f22449bd639966a3bd1dd44356ab423c5ccee48a
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2025-07-13 22:56:08 -0300

    Permitir adicionar novo produto

--- projeto/src/main/java/com/bd/ufrn/projeto/controllers/EstoquePageController.java
+++ projeto/src/main/java/com/bd/ufrn/projeto/controllers/EstoquePageController.java
@@ -1,11 +1,14 @@
 package com.bd.ufrn.projeto.controllers;
 
 import com.bd.ufrn.projeto.dtos.BreadcrumbItem;
+import com.bd.ufrn.projeto.dtos.ProdutoDTO;
 import com.bd.ufrn.projeto.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -32,5 +35,24 @@ public class EstoquePageController {
         model.addAttribute("produtos", produtoService.getAllAsDto());
 
         return "estoque/lista-produtos";
+    }
+
+    @GetMapping("/produtos/novo")
+    public String novoProdutoForm(Model model) {
+        List<BreadcrumbItem> breadcrumbs = new ArrayList<>();
+        breadcrumbs.add(new BreadcrumbItem("Produtos", "/estoque/produtos", false));
+        breadcrumbs.add(new BreadcrumbItem("Novo", null, true));
+
+        model.addAttribute("breadcrumbs", breadcrumbs);
+        model.addAttribute("pageTitle", "Novo Produto");
+        model.addAttribute("produto", new ProdutoDTO(null, "", 0.0, 0));
+
+        return "estoque/novo-produto";
+    }
+
+    @PostMapping("/produtos/novo")
+    public String salvarNovoProduto(@ModelAttribute("produto") ProdutoDTO produtoDTO) {
+        produtoService.create(produtoDTO);
+        return "redirect:/estoque/produtos";
     }
 }
